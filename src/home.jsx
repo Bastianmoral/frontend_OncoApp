559543e9a50fb932551dc0acf4cf87e6029c27cc
Bastianmoral <bastihc@gmail.com>
2024-04-10 17:45:23 -0400

    First Blood

---
+++ src/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+
+// Seguir leyendo https://medium.com/@galohernandez/vite-react-react-router-dom-the-latest-way-312ee887197e
